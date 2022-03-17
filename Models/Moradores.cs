3a6a15a9b35dcb05a477710579ad7290ebb93675
Douglas Vinicius Vicente <douglas.vinicius001@gmail.com>
2022-03-17 16:24:33 -0300

    two commit

--- Models/Moradores.cs
+++ Models/Moradores.cs
@@ -1,50 +1,25 @@
-using System;
-namespace Teste_3_GFT_new_2.Models
+namespace TESTE_3_GFT.Models
 {
-    public class Moradores
+    public class Morador
     {
-        private int Num { get; set; }
-        private int Tel{ get; set; }
+        private string Telefone;
 
-        private int CasaImpar { get; set; }
-    
-        public void Teste1()
-        {               
-                        string Casa;
-                        int CasaImpar;
-                        CasaImpar=0;
+        private int NumeroResidencia;
 
-                                              
-                        for (int i = 1; i <= 4; i++)
-                        {
-                        Console.WriteLine($"A casa {i} tem cachorro? Pressione [S] se Sim e [N] se não tiver cachorros: ");
-                        Casa = Console.ReadLine();
-                        Casa = Casa.ToUpper();
-                        Console.WriteLine();
-                           
-                            if (Casa == "N")
-                            {
-                            int [] Num= new int [5];
-                            Console.WriteLine($"Digite o numero da {i}º casa: ");
-                            Num[i] = int.Parse(Console.ReadLine());
-                        
-                            int [] Tel =new int [5];
-                            Console.WriteLine($"Digite o telefone da casa {i}º: ");
-                            Tel[i] = int.Parse(Console.ReadLine());
-                                if (Num[i] % 2==1)
-                                {
-                                    CasaImpar = CasaImpar +1; //Conta quantos ímpares foram somados.
-                                }
-                            }
-                            if (Casa == "S")
-                            {
-                            Console.WriteLine("Você correu desesperadamente para a próxima casa! ");
-                            Console.WriteLine();
-                            }
-                        }
-                        Console.WriteLine($"Foram entregues {CasaImpar} corresondencias em casas de numeros ímpares. ");
-        }               
-    
-    
+        public Morador(string tel,int n)
+        {
+        this.Telefone = tel;
+
+        this.NumeroResidencia = n;
+        }
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public int getNumeroResidencia()
+        {
+            return NumeroResidencia;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,42 @@
-﻿using Teste_3_GFT_new_2.Models;
-using System;
-namespace Teste_3_GFT_new_2
-{
-      class program
-      {
+﻿using System;
+using System.Collections.Generic;
+using TESTE_3_GFT.Models;
+using static System.Console;
 
-            public static void Main(string[] args)
-            {     Console.WriteLine("-----------------------");
-                  Console.WriteLine("     Carteiro    ");
-                  Console.WriteLine("-----------------------");
-                  Console.WriteLine("Neste projeto você percorrerá 4 casas entregando correspondencias. Se tiver cachorros corra!");
-                  Console.WriteLine("______________________________________________________________________________________________");
-                  Console.WriteLine();
-                  Moradores m1 =new Moradores();
-                  m1.Teste1();
+namespace TESTE_3_GFT
+{
+      class Program
+      {
+            public static void Main(string[]args)
+            {
+                  var Moradores = new List<Morador>();
+                  Moradores.Add(new Morador("36958847",1));
+                  Moradores.Add(new Morador("36958475",2));
+                  Moradores.Add(new Morador("15478844",3));
+                  Moradores.Add(new Morador("36914459",4));
+                  Moradores.Add(new Morador("99111847",5));
+                  Moradores.Add(new Morador("991012923",6));
+
+                  int CasasImparesEntregues = 0;
+                  foreach (var morador in Moradores)
+                  {
+                      WriteLine($"\n\n Na residencia numero {morador.getNumeroResidencia()}? tem cachorro? [S]sim / [N]ao");
+                      var cachorroExists = ReadLine();
+                      if (cachorroExists.Equals("n", StringComparison.InvariantCultureIgnoreCase))
+                      {
+                        if (morador.getNumeroResidencia() % 2 == 1)
+                        {
+                        CasasImparesEntregues++;
+                        }
+                      }
+                        else
+                        {
+                              WriteLine($"Correio nao foi entregue na casa numero {morador.getNumeroResidencia()}");
+                        }
+
+                      
+                  }WriteLine($"O numero de casas ímpares que receberam sua correspôndencia foi de: {CasasImparesEntregues}");
+                  
             }
       }
-      
-} 
+}
